75def8777be20ca524d36e6cef3096277425d9e7
Shustriy90 <shustriy90@gmail.com>
2018-04-26 01:04:28 +0300

    скрипт изменяющий цену

--- js/common.js
+++ js/common.js
@@ -1,13 +1,26 @@
+$(document).ready(function(){
+  $('.reviews-slider').slick();
+});
 
-    $(document).ready(function(){
-      $('.reviews-slider').slick();
-    });
-    
-	var num = 140; //number of pixels before modifying styles
-	$(window).bind('scroll', function () {
-	    if ($(window).scrollTop() >= num) {
-	        $('.menu').addClass('menu-dark');
-	    } else {
-	        $('.menu').removeClass('menu-dark');
-	    }
-	});
+var num = 140; //number of pixels before modifying styles
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() >= num) {
+        $('.menu').addClass('menu-dark');
+    } else {
+        $('.menu').removeClass('menu-dark');
+    }
+});
+
+var basePrice = 9800;
+var currentPrice = basePrice;
+
+
+function updatePrice(chk, value) {
+  if (chk.checked) {
+    currentPrice += value;
+  }
+  else {
+    currentPrice -= value;
+  }
+  document.querySelector("#price").textContent = currentPrice;
+}
